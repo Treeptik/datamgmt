2f2b3f313474c62f31be25f9d5f9cb092d2f89a8
sebastienmusso <s.musso@treeptik.fr>
2017-02-13 14:34:24 +0100

    add gitignore

--- logging/start.go
+++ logging/start.go
@@ -10,7 +10,6 @@ import (
   "github.com/docker/docker/api/types/events"
   "github.com/docker/docker/api/types/container"
   networktypes "github.com/docker/docker/api/types/network"
-  //"reflect"
 )
 
 func StartLogging(client *client.Client, message events.Message) {
@@ -23,22 +22,13 @@ func StartLogging(client *client.Client, message events.Message) {
   if err != nil  {
     if strings.Contains(err.Error(), "No such container:") {
       fmt.Println("Logging container doesn't exist lets create and start it")
-      //fmt.Println(message.Actor.Attributes["application-type"])
       labels := make(map[string]string)
       labels["origin"] = "datamgmt"
       labels["application-type"] = "filebeat"
-      //test := network.EndpointSettings{ NetworkID: "fd9af838d33f" }
-      /*network := map[string]interface{}{
-		    "NetworkingConfig": networktypes.NetworkingConfig{
-			    EndpointsConfig: map[string]*networktypes.EndpointSettings{
-				    "datamgmt": {},
-			    },
-		    },
-	    }*/
       network := map[string]*networktypes.EndpointSettings{
 			    "datamgmt": {},
 		  }
-      r, err := client.ContainerCreate(context.Background(), &container.Config{Hostname: containername, Labels: labels, Image: "cloudunit/datamgmt-filebeat:latest", Env: []string{"APPLICATION_TYPE="+message.Actor.Attributes["application-type"]}}, &container.HostConfig{ VolumesFrom: []string{basecontainer}}, &networktypes.NetworkingConfig{ EndpointsConfig: network}, containername)
+      r, err := client.ContainerCreate(context.Background(), &container.Config{Hostname: basecontainer, Labels: labels, Image: "cloudunit/datamgmt-filebeat:latest", Env: []string{"APPLICATION_TYPE="+message.Actor.Attributes["application-type"]}}, &container.HostConfig{ VolumesFrom: []string{basecontainer}}, &networktypes.NetworkingConfig{ EndpointsConfig: network}, containername)
       if err != nil {
         fmt.Println("Could not create filebeat container", err)
       } else {
