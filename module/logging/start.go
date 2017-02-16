581c2d21d88ae6625b1fcb4e134b9c8d465979d3
sebastienmusso <s.musso@treeptik.fr>
2017-02-16 10:13:51 +0100

    Add testing for common, set gid in filebeat container with the gid of the applicative one in order to reduce permission

--- module/logging/start.go
+++ module/logging/start.go
@@ -4,6 +4,7 @@ import (
   "bytes"
   "fmt"
   "strings"
+  "bufio"
   "github.com/docker/docker/api/types"
   "github.com/docker/docker/api/types/container"
   "github.com/docker/docker/api/types/events"
@@ -22,13 +23,40 @@ func StartLogging(client *client.Client, message events.Message) {
   if err != nil  {
     if strings.Contains(err.Error(), "No such container:") {
       fmt.Println("Logging container doesn't exist lets create and start it")
+      execid, err := client.ContainerExecCreate(context.Background(), basecontainer, types.ExecConfig{
+		    Tty:          true,
+        AttachStdout: true,
+        Cmd:          []string{"id", "-u"},
+	    })
+      var gid string
+      if err != nil {
+        fmt.Println("Error When creating exec task")
+      } else {
+          output, err := client.ContainerExecAttach(context.Background(), execid.ID, types.ExecConfig{
+            Tty:          true,
+            AttachStdout: true,
+            Cmd:          []string{"id", "-u"},
+          })
+          if err != nil {
+            fmt.Println("Error on ExecAttach")
+          } else {
+            scanner := bufio.NewScanner(output.Reader)
+            for scanner.Scan() {
+              gid = scanner.Text()
+	          }
+            if err != nil {
+              fmt.Println("Err while converting uidtemp to uid", err)
+            }
+          }
+      }
+      user := "10000:"+gid
       labels := make(map[string]string)
       labels["origin"] = "datamgmt"
       labels["application-type"] = "filebeat"
       network := map[string]*networktypes.EndpointSettings{
 			    "datamgmt": {},
 		  }
-      r, err := client.ContainerCreate(context.Background(), &container.Config{Hostname: basecontainer, Labels: labels, Image: "cloudunit/datamgmt-filebeat:latest", Env: []string{"APPLICATION_TYPE="+message.Actor.Attributes["application-type"]}}, &container.HostConfig{ VolumesFrom: []string{basecontainer}}, &networktypes.NetworkingConfig{ EndpointsConfig: network}, containername)
+      r, err := client.ContainerCreate(context.Background(), &container.Config{Hostname: basecontainer, User: user, Labels: labels, Image: "cloudunit/datamgmt-filebeat:latest", Env: []string{"APPLICATION_TYPE="+message.Actor.Attributes["application-type"]}}, &container.HostConfig{ VolumesFrom: []string{basecontainer}}, &networktypes.NetworkingConfig{ EndpointsConfig: network}, containername)
       if err != nil {
         fmt.Println("Could not create filebeat container", err)
       } else {
